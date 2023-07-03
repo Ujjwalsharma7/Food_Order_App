8e5eef741bee55cb5c35606af10531c008065108
Ujjwal Sharma <ujjwalsharma1772003@gmail.com>
2023-07-03 15:26:23 +0530

    added mealitemform

---
+++ src/components/Meals/MealItem/MealItemform.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import classes from './MealItemform.module.css'
+function MealItemform(props) {
+  return (
+   <form className={classes.form}>
+    <input />
+    <button>
+        + Add
+    </button>
+   </form>
+  )
+}
+
+export default MealItemform
