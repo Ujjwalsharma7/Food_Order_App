f8b54f4351a992f430f73bf1c163c217b9dfc95c
Ujjwal Sharma <ujjwalsharma1772003@gmail.com>
2023-07-03 12:18:51 +0530

    added css to header image

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -8,7 +8,7 @@ function Header() {
         <h1>UjjuMeals</h1>
         <button>Cart</button>
     </header>
-    <div>
+    <div className={classes['main-image']}>
         <img src={mealsImage} alt='A table full of delicious food' />
     </div>
    </React.Fragment>
