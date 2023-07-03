71c5058b81d293739d0c78ea535b68788f5676f9
Ujjwal Sharma <ujjwalsharma1772003@gmail.com>
2023-07-03 13:59:15 +0530

    added headercartbtn

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import mealsImage from '../../assets/meals.jpg'
 import classes from './Header.module.css'
+import HeaderCartbtn from './HeaderCartbtn'
 function Header() {
   return (
    <React.Fragment>
     <header className={classes.header}>
         <h1>UjjuMeals</h1>
-        <button>Cart</button>
+  <HeaderCartbtn />
     </header>
     <div className={classes['main-image']}>
         <img src={mealsImage} alt='A table full of delicious food' />
--- src/components/Layout/HeaderCartbtn.js
+++ src/components/Layout/HeaderCartbtn.js
@@ -1,11 +1,16 @@
 import React from 'react'
-
+import CartIcon from '../Cart/CartIcon'
+import classes from './HeaderCartbtn.module.css'
 function HeaderCartbtn() {
   return (
-    <button>
-        <span></span>
-        <span></span>
-        <span></span>
+    <button className={classes.button}>
+        <span>
+            <CartIcon />
+        </span>
+        <span> Your Cart</span>
+        <span>
+            3
+        </span>
     </button>
   )
 }
