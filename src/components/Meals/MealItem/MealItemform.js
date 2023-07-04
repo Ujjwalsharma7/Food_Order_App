8a7070e0228e18171f59137f0a4d793affbf7233
Ujjwal Sharma <ujjwalsharma1772003@gmail.com>
2023-07-04 16:05:01 +0530

    added input to ui

--- src/components/Meals/MealItem/MealItemform.js
+++ src/components/Meals/MealItem/MealItemform.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import classes from './MealItemform.module.css'
+import Input from '../../UI/input'
 function MealItemform(props) {
   return (
    <form className={classes.form}>
-    <input />
+<Input label="Amount" input = {{
+  id: 'amount_' + props.id,
+  type: 'number',
+  min: '1',
+  max: '5',
+  step: '1',
+  defaultValue: '1'
+}}/>
     <button>
         + Add
     </button>
